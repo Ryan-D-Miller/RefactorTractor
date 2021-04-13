2b2da930180bd60e8ea4f91db6bd9009e1585b56
Ryan Miller <ryandmiller3521@gmail.com>
2021-04-13 17:36:23 -0600

    Add Function to Pantry to see if you have all ingredients

--- src/pantry.js
+++ src/pantry.js
@@ -2,6 +2,26 @@ class Pantry {
   constructor(userIngredients) {
     this.contents = userIngredients;
   }
+
+  checkMeal(recipe) {
+    let haveIngredient = true;
+    let missingIngredients = [];
+    recipe.ingredients.forEach(ingredient => {
+      let ingredientIndex = this.contents.findIndex(element => element.ingredient === ingredient.id);
+      if(ingredientIndex === -1 || this.contents[ingredientIndex].amount < ingredient.quantity.amount) {
+        haveIngredient = false;
+        let amountDifference = ingredient.quantity.amount;
+        if(ingredientIndex !== -1) {
+          amountDifference = ingredient.quantity.amount - this.contents[ingredientIndex].amount;
+        }
+        missingIngredients.push({'name': ingredient.name, 'amount': amountDifference});
+      }
+    });
+    if(haveIngredient){
+      return haveIngredient;
+    }
+    return missingIngredients;
+  }
 }
 
 export default Pantry;
